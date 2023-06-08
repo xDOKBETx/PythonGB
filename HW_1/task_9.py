bc55160bd318705546bb12430877436d9132abc4
Aleksey Valentinov <ds83351@chebnet.com>
2023-06-08 23:32:59 +0300

    the task #9 from HW #1 was completed

---
+++ HW_1/task_9.py
@@ -0,0 +1,18 @@
+'''Задание №9
+
+Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке. 
+'''
+
+for i in range(2, 11):
+    for j in range(2, 6):
+        multiplication = i * j
+        print(f"{j} x {i} = {multiplication}\t", end="")
+    print()
+
+print()
+
+for i in range(2, 11):
+    for j in range(6, 10):
+        multiplication = i * j
+        print(f"{j} x {i} = {multiplication}\t", end="")
+    print()

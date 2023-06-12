4d3d2fbf8770ef14188b3f15a96bc546c0c8a967
Aleksey Valentinov <ds83351@chebnet.com>
2023-06-12 09:50:08 +0300

    the change task_7

--- HW_2/task_7.py
+++ HW_2/task_7.py
@@ -5,22 +5,15 @@
 
 
 def to_hexadecimal(number):
-    # Словарь для преобразования остатка в шестнадцатеричный символ
-    hexadecimal_dict = {
-        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7',
-        8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'
-    }
+    hex_digits = "0123456789ABCDEF"
+    hexadecimal = ""
 
     if number == 0:
-        return '0'
+        return "0"
 
-    hexadecimal = ''
     while number > 0:
-        # Получение остатка от деления на 16
         remainder = number % 16
-        # Добавление соответствующего шестнадцатеричного символа в начало строки
-        hexadecimal = hexadecimal_dict[remainder] + hexadecimal
-        # Деление числа на 16 для получения следующего разряда
+        hexadecimal = hex_digits[remainder] + hexadecimal
         number //= 16
 
     return hexadecimal
@@ -32,6 +25,6 @@ hexadecimal = to_hexadecimal(number)
 print(
     f"Шестнадцатеричное представление числа: {hexadecimal}")
 
-# Проверка с использованием функции hex
-hexadecimal_hex = hex(number).lstrip('0x').upper()
-print(f"Проверка: {hexadecimal == hexadecimal_hex}")
+# Проверка с использованием функции hex()
+hex_check = hex(number)[2:].upper()
+print(f"Проверка: {hexadecimal == hex_check}")

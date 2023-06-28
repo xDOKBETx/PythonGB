30fd24bd2993e35a12cf50890eb067d60945d399
Aleksey Valentinov <ds83351@chebnet.com>
2023-06-28 22:12:38 +0300

    the main from HW_6 was completed

---
+++ HW_6/main.py
@@ -0,0 +1,10 @@
+import chess_module
+
+successful_arrangements = chess_module.generate_random_queens()
+
+if len(successful_arrangements) > 0:
+    print("Успешные расстановки: ")
+    for arrangement in successful_arrangements:
+        print(arrangement)
+else:
+    print("Нет удачных расстановок.")

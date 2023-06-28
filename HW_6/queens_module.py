d5f0e4c1e46f26f55612fad482cdce9e0b66716e
Aleksey Valentinov <ds83351@chebnet.com>
2023-06-28 22:15:31 +0300

    the queens_module from HW_6 was completed

---
+++ HW_6/queens_module.py
@@ -0,0 +1,43 @@
+from random import randint
+
+
+def are_queens_safe(queens):
+    """
+    Проверяет, безопасно ли расположены ферзи на доске.
+
+    Аргументы:
+    queens (list): Список координат ферзей на доске.
+
+    Возвращает:
+    bool: True, если ферзи не бьют друг друга, иначе False.
+    """
+    for a, b in ((a, b) for a in queens for b in queens if a[0] < b[0]):
+        if abs(a[0] - b[0]) == abs(a[1] - b[1]):
+            return False
+    return True
+
+
+def generate_random_queens(num_queens, num_attempts):
+    """
+    Генерирует случайные расстановки ферзей на доске и возвращает успешные расстановки.
+
+    Аргументы:
+    num_queens (int): Количество ферзей.
+    num_attempts (int): Количество попыток генерации случайных расстановок.
+
+    Возвращает:
+    list: Список успешных расстановок ферзей на доске.
+    """
+    successful_arrangements = []
+
+    for _ in range(num_attempts):
+        queens = [(i, randint(1, num_queens))
+                  for i in range(1, num_queens + 1)]
+
+        if are_queens_safe(queens):
+            successful_arrangements.append(queens)
+
+        if len(successful_arrangements) == 4:
+            break
+
+    return successful_arrangements
